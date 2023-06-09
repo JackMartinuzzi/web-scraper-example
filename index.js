0524cda5f8379a811cc17ecc9af7bcf42a0fb9fd
JackMartinuzzi <jjmartinuzzi@gmail.com>
2023-06-08 20:34:54 -0600

    Example complete

---
+++ index.js
@@ -0,0 +1,31 @@
+// server
+const PORT = 3000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express();
+// set target url
+const url = 'https://www.youtube.com/';
+
+// send get request to url
+axios.get(url)
+  .then(res => {
+    // response data is page html
+    const html = res.data;
+    // load html data for parsing
+    const $ = cheerio.load(html);
+    // declare empty array to store scraped url data
+    const urls = [];
+
+    // look for links on page
+    $('a', html).each(function() {
+      // push links and titles to array
+      urls.push({
+        title: $(this).text(),
+        url: $(this).attr('href')
+      });
+    });
+
+    console.log(urls);
+  });
